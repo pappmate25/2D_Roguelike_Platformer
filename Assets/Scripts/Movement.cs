8da70aa1627e3d2aad842baa2a451890d81acd8e
matepapp25 <mate.papp_contract@playableworlds.com>
2024-04-23 15:48:35 +0200

    basic_movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,9 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float movementspeed = 8f;
+    [SerializeField] float jumpspeed = 10f;
+
+    Vector2 moveInput;
+    Rigidbody2D rb;
+    Animator animator;
+    BoxCollider2D boxcollider;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        boxcollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+   
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed && boxcollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            rb.velocity += new Vector2(0f, jumpspeed);
+        }
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * movementspeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;       
+        animator.SetBool("isRunning", playerHasHorizontalSpeed);
+
+
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;  //Mathf.Epsilon tulajdonképpen = 0
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
+    }
 
     
+
+    
+
 }
