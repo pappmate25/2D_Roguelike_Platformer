6856242bce4d6b523acbaa13fe020d40916a8268
Gergely Bod <bodge04@gmail.com>
2024-05-07 16:18:13 +0200

    Level 1 BG music import

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+}
